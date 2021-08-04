430dbbda7d7c510a2b273941f0f8ea608f124414
Seoyeon Kim <seoyeonkim815@gmail.com>
2021-08-04 10:33:48 +0900

    [Update] Added routing to each page

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,20 +1,13 @@
-import { useEffect, useRef } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import axios from 'axios'
 
 export default function Home() {
   const ref:any = useRef()
-  useEffect(()=> {
-    axios.get(`https://acnhapi.com/v1/houseware/acoustic_guitar`)
-    .then (res => {
-      ref.current.setAttribute('src', res.data[0].image_url) 
-
-    })
-    .catch(err=> console.log(err))
-  }, [])
+  const [houseware, setHouseware] = useState([])
+  
   return (
     <div>
       Hello
-      <img ref={ref} alt="Guitar"></img>
     </div>
   )
 }
